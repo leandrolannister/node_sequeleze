e2be60e1da42820e2b73d209505a0be447b08bfd
leandrolannister <leandrohendrix@gmail.com>
2021-05-11 05:45:31 -0300

    Acerto chamada model Pessoa

--- api/controllers/PessoaController.js
+++ api/controllers/PessoaController.js
@@ -1,9 +1,9 @@
-const database = require('../models/pessoas');
+const database = require('../models');
 
 class PessoaController{
     static async show(req, res){
        try{
-        const pessoas = await database.pessoas.findAll();
+        const pessoas = await database.Pessoas.findAll();
         return res.status(200).json(pessoas);
        }catch(error){
            return res.status(500).json(error.message);
